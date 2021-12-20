33da99a4c2fb4c223f546801637f50839760187e
Paul <paul.benier@efrei.net>
2021-12-20 18:05:35 +0100

    start

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Title of the web page</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        
+        <header>  <!-- ##### HEADER ##### -->
+            header
+        </header> <!-- ##### end - HEADER ##### -->
+        
+        <div id="page">
+            page
+        </div>
+
+        <footer>  <!-- ##### FOOTER ##### -->
+            footer
+        </footer> <!-- ##### end - FOOTER ##### -->
+
+    </body>
+</html>
