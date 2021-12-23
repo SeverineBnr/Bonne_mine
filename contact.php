33adafc7df20743d5340493dea59aec026d4437c
BENIER <sbenier@icloud.com>
2021-12-23 17:10:01 +0100

    envoi de mail

--- contact.php
+++ contact.php
@@ -13,7 +13,85 @@
         </header> <!-- ##### end - HEADER ##### -->
         
         <div id="page">
-            L'Epicerie Bonne Mine - Contact
+            L'Epicerie Bonne Mine - Contact <br>
+            <?php
+                if(isset($_POST['contact'])){
+                    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['objet']) && isset($_POST['contenu']) && isset($_POST['civilite'])){
+                        if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email']) && !empty($_POST['objet']) && !empty($_POST['contenu']) && !empty($_POST['civilite'])){
+                            $civilite = $_POST['civilite'];
+                            $nom = $_POST['nom'];
+                            $prenom = $_POST['prenom'];
+                            $email = $_POST['email'];
+                            $objet = $_POST['objet'];
+                            $contenu = $_POST['contenu'];
+                            echo $nom . " - " . $prenom . " - " . $email . " - " . $objet . " - " . $contenu . "<br>";
+                        }
+                        else{
+                            echo("Le formulaire de contact n'est pas complet");
+                        }
+                    }
+                    else{
+                        echo("Le formulaire de contact n'est pas complet");
+                    }
+                }
+            ?>
+
+
+            <form action="contact.php" method="POST"> 
+                <input type="radio" name="civilite" id="m" value="M.">
+                <label for="m">M.</label><br>
+
+                <input type="radio" name="civilite" id="mme" value="Mme">
+                <label for="mme">Mme</label><br>
+
+                <label for="nom">Nom :</label><br>
+                <input type="text" name="nom" id="nom"><br>
+                
+                <label for="prenom">Prénom :</label><br>
+                <input type="text" name="prenom" id="prenom"><br>
+                
+                <label for="email">Email :</label><br>
+                <input type="email" name="email" id="email" required><br>
+
+                <label for="objet">Objet :</label><br>
+                <input type="text" name="objet" id="objet"><br>
+
+                <label for="contenu">Contenu de votre message :</label><br>
+                <textarea name="contenu" id="contenu"></textarea><br>
+            
+                <button type="submit" name="contact" value="Envoyer">Envoyer</button>
+            </form>
+
+            
+            <?php
+                ini_set("SMTP", "ssl0.ovh.net");
+                ini_set("sendmail_from", "severine@benier.dev");
+                
+                $headers ='From: severine@benier.dev'."\n";
+                $headers .='Reply-To: severine@benier.dev'."\n";
+                $headers .='Content-Type: text/plain; charset="iso-8859-1"'."\n";
+                $headers .='Content-Transfer-Encoding: 8bit';
+                $headers.= 'Content-Type: text/html; charset="utf-8"'."\n";
+                
+                $objetMail = $civilite . " " . $prenom . "  " . $nom . " vous a contacté";
+                $contenuMail = "Objet : ". $objet . "\n\n" . "Message : " . $contenu;
+                
+                if (
+                    mail(
+                        'severine@benier.dev', 
+                        $objetMail, 
+                        $contenuMail, 
+                        $headers
+                    ) 
+                ){
+                    //echo 'ok';
+                    
+                } else {
+                    echo 'erreur';
+                }
+                //echo "Check your email now....<br/>";
+            ?>
+            Merci de nous avoir contactés. Nous vous contacterons très bientôt.
         </div>
 
         <footer>  <!-- ##### FOOTER ##### -->
--- index.php
+++ index.php
@@ -13,7 +13,25 @@
         </header> <!-- ##### end - HEADER ##### -->
         
         <div id="page">
-            L'Epicerie Bonne Mine
+            <a href="index.php"><img src="img/Logo.png" alt="Logo de L'Épicerie Bonne Mine"></a>
+            ECORESPONSABLE PRODUITS LOCAUX EN VRAC<br>
+            Une épicerie de produits authentiques préparés par des femmes et des hommes respectueux de notre alimentation et de notre planète.
+
+            Pourquoi L'Epicerie Bonne Mine ?
+
+            <p>Un lieu de vie<br>
+            Nous faisons nos courses apaisé.e.s.<br>
+            Nous avons une écoute bienveillante et discrète. Nous nous rencontrons et nous avançons.<br>
+            Nous avons bonne mine</p>
+
+            <p>Un lieu éthique<br>
+            Nous consommons des produits sains. Nous limitons nos déchets en achetant le produit et non son contenant. Nous nous sentons responsables de notre consommation. Nous payons un prix juste.<br>
+            Nous avons bonne mine</p>
+            
+            <p>Un lieu ancré<br>
+            Dans ce village d’irréductibles, nous défendons notre territoire, nos idées.<br>
+            Nous avons bonne mine</p>
+
         </div>
 
         <footer>  <!-- ##### FOOTER ##### -->
